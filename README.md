384b8e3c2c3f7695935aeeebe4f59a2b2810d6ea
gregory6z <gregoryrag@gmail.com>
2021-03-24 23:21:48 +0100

    TransactionContext

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Modal & Forms
+# Context 
 
-### Lister les transactions et les seeds
+partage d'état entre divers composants     
+d'application, quel que soit leur emplacement
+
+il y a deux manières de partager l'état, par le composant parent,     
+est utilisé lorsqu'il est plus complexe ou       
+lorsqu'il n'est pas au bon endroit en utilisant le contexte
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 
 import { GlobalStyle } from './styles/global'
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root')
 
@@ -20,7 +21,7 @@ export function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-      <>
+      <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       
       <Dashboard />
@@ -30,7 +31,7 @@ export function App() {
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
 
  
-      </>
+      </TransactionsContext.Provider>
   );
 }
 
